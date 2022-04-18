c24af218ef483baa0f178ae466796307d1c00954
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-04-18 20:46:27 +0900

    implement logout feature

--- src/main/java/jp/co/kokou/issuetracker/config/AuthConfig.java
+++ src/main/java/jp/co/kokou/issuetracker/config/AuthConfig.java
@@ -1,5 +1,6 @@
 package jp.co.kokou.issuetracker.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,7 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -18,7 +21,7 @@ public class AuthConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().usernameParameter("username").passwordParameter("password")
                 .loginPage("/auth/login").permitAll()
                 .and()
-                .logout()
+                .logout().logoutSuccessUrl("/")
                 .logoutUrl("/auth/logout").permitAll();
     }
 }
